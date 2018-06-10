5851fca1f27b443ae2a7edafc03d530d6ffca615
Cristian Dinu <cristian.dinu@readfwd.com>
2018-06-10 13:08:09 +0100

    First draft with content and mini-website-generator

---
+++ pages/credits/00-credits.md
@@ -0,0 +1,21 @@
+## Credits
+
+### Icons
+
+- [Tyrannosaurus Rex](https://thenounproject.com/term/tyrannosaurus-rex/375751/) by Abel Tan from the [Noun Project](https://thenounproject.com)
+
+### Inspiration:
+
+- [Gina Trapani](https://ginatrapani.org/)'s amazing personal website
+
+### Technologies used:
+
+- [Handlebars](http://handlebarsjs.com/)
+- [Markdown](https://daringfireball.net/projects/markdown/) and [Marked](https://github.com/markedjs/marked)
+- [Node.js](https://nodejs.org)
+
+### License
+
+- This site is open source, under a [MIT License](https://opensource.org/licenses/MIT)
+- Source code available on [GitHub](https://github.com/cdinu/cdinu.github.io)
+- Hosted free at [github.io](https://pages.github.com/)
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
+const handlebars = require('handlebars');
+
+const defaults = {
+  title: 'Cristian Dinu',
+  description: 'Cristian Dinu\'s official page. Short bio and some of the latest noteworthy happenings',
+  outputDir: path.join(__dirname, '..'),
+  style: fs.readFileSync(path.join(__dirname, 'style.css')).toString(),
+  baseUrl: 'http://localhost:3000',
+}
+
+layout = handlebars.compile(
+  fs.readFileSync(
+    path.join(__dirname, 'layout.hbs')
+  ).toString()
+);
+
+const build = () => {
+  const pagesDir = path.join(__dirname, '..', 'pages');
+  const pages = fs.readdirSync(pagesDir);
+  pages.forEach(page => {
+    const config = require(path.join(pagesDir, page, 'config.json'));
+
+    const body = fs
+      .readdirSync(path.join(pagesDir, page))
+      .filter(fileName => fileName.match(/\.md$/i))
+      .sort()
+      .map(fileName => fs.readFileSync(path.join(pagesDir, page, fileName)).toString())
+      .map(fileContents => marked(fileContents))
+      .join('\n');
+
+    const canonical = page == 'index' ? defaults.baseUrl : `${defaults.baseUrl}/${page}.html`
+    const output = layout({ ...defaults, ...config, canonical, body });
+
+    fs.writeFile(
+      path.join(defaults.outputDir, `${page}.html`),
+      output,
+      ()=> console.log(`Generated ${page}.`)
+    );
+  });
+}
+
+console.log(`Using ${defaults.outputDir} to output your files`);
+build();
